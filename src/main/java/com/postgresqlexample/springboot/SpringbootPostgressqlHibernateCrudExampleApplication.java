fc84ef2edf21188d6452b5dc9e5c5966649fe1b7
Dragon <solootronombre@gmail.com>
2020-06-08 14:39:29 +0800

    initial code

---
+++ src/main/java/com/postgresqlexample/springboot/SpringbootPostgressqlHibernateCrudExampleApplication.java
@@ -0,0 +1,13 @@
+package com.postgresqlexample.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootPostgressqlHibernateCrudExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootPostgressqlHibernateCrudExampleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/postgresqlexample/springboot/SpringbootPostgressqlHibernateCrudExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.postgresqlexample.springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootPostgressqlHibernateCrudExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
